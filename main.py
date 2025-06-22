33aa22db79f6736b10fa1f8a6c3204a7cc8a159d
AndreyBatuev <batushkatas4it@yandex.ru>
2025-06-23 02:52:27 +0300

    Add 8 color palete

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 
 from PIL import Image
+def choseSymbol(brightness):
+    palete2 = "  ··──││░░▒▒▓▓██" 
+    selectPalete = palete2
+    if brightness < 15:
+        return selectPalete[0]
+    elif brightness < 31:
+        return selectPalete[1]
+    elif brightness < 47:
+        return selectPalete[2]
+    elif brightness < 63:
+        return selectPalete[3]
+    elif brightness < 79:
+        return selectPalete[4]
+    elif brightness < 95:
+        return selectPalete[5]
+    elif brightness < 111:
+        return selectPalete[6]
+    elif brightness < 127:
+        return selectPalete[7]
+    elif brightness < 143:
+        return selectPalete[8]
+    elif brightness < 159:
+        return selectPalete[9]
+    elif brightness < 175:
+        return selectPalete[10]
+    elif brightness < 191:
+        return selectPalete[11]
+    elif brightness < 207:
+        return selectPalete[12]
+    elif brightness < 223:
+        return selectPalete[13]
+    elif brightness < 239:
+        return selectPalete[14]
+    elif brightness < 255:
+        return selectPalete[15] 
 
 def DEBUG_print_data_pixels(ImageName):
     image = Image.open(ImageName)  
@@ -11,7 +46,7 @@ def DEBUG_print_data_pixels(ImageName):
         x = 0
         while x != width:
             r, g, b, a = pixels[x, y]  
-            brightness_simple = (r + g + b) // 3  # Среднее
+            brightness_simple = (r + g + b) // 3  
             print(f"{brightness_simple}\t",end="")
             x += 1
         print("")
@@ -31,12 +66,7 @@ def createImage(imageName):
 
             
             brightness_simple = (r + g + b) // 3 
-            if brightness_simple == 0:
-                print(" ", end="")
-            elif brightness_simple == 13:
-                print("|", end="")
-            elif brightness_simple == 160:
-                print("Y", end="")
+            print (choseSymbol(brightness_simple), end="")
 
             x += 1
             
@@ -48,7 +78,9 @@ def createImage(imageName):
 
 
 def main():
-    DEBUG_print_data_pixels("Molly.png")
     createImage("Molly.png")
+    
+
+
 if __name__=="__main__": 
     main() 
