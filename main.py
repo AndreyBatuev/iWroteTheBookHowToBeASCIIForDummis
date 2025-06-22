bc49cd0faf49cce3c9b270c95a6ead9ddf9e3f98
AndreyBatuev <batushkatas4it@yandex.ru>
2025-06-23 01:27:00 +0300

    rotate image to right side

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def createImage():
     image = Image.open("Molly.png")  
     pixels = image.load()
     width, height = image.size
-    x = 0
-    while x != width:
-        y = 0
-        while y != height:
-            r, g, b, a = pixels[x, y] 
+    y = 0
+    while y != height:
+        x = 0
+        while x != width:
+            if image.mode == 'RGBA':
+                r, g, b, a = pixels[x, y]
+            else:
+                r, g, b = pixels[x, y]
+
+            
             brightness_simple = (r + g + b) // 3 
             if brightness_simple == 0:
                 print(" ", end="")
@@ -19,13 +24,13 @@ def createImage():
             elif brightness_simple == 160:
                 print("Y", end="")
 
-            y += 1
+            x += 1
             
 
 
         print("")
         
-        x += 1
+        y += 1
 
 
 def main():
