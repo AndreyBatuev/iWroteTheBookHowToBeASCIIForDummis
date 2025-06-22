2555311041d7dbd4f65a302cf730b654df18e2b4
AndreyBatuev <batushkatas4it@yandex.ru>
2025-06-23 01:16:27 +0300

    Add main func, remove image create in func

--- main.py
+++ main.py
@@ -2,27 +2,34 @@
 from PIL import Image
 
 
-image = Image.open("Molly.png")  
-pixels = image.load()
-width, height = image.size
-x = 0
-while x != width:
-    y = 0
-    while y != height:
-        r, g, b, a = pixels[x, y] 
-        brightness_simple = (r + g + b) // 3 
-        if brightness_simple == 0:
-            print(" ", end="")
-        elif brightness_simple == 13:
-            print("|", end="")
-        elif brightness_simple == 160:
-            print("Y", end="")
+def createImage():
+    image = Image.open("Molly.png")  
+    pixels = image.load()
+    width, height = image.size
+    x = 0
+    while x != width:
+        y = 0
+        while y != height:
+            r, g, b, a = pixels[x, y] 
+            brightness_simple = (r + g + b) // 3 
+            if brightness_simple == 0:
+                print(" ", end="")
+            elif brightness_simple == 13:
+                print("|", end="")
+            elif brightness_simple == 160:
+                print("Y", end="")
 
-        y += 1
+            y += 1
+            
+
+
+        print("")
         
+        x += 1
 
 
-    print("")
-       
-    x += 1
-    
+def main():
+    createImage()
+
+if __name__=="__main__": 
+    main() 
